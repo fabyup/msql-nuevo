18d0d7b1efbe21cfd42808d4b170c0f00c71e53b
PC 1 <>
2024-06-13 20:26:50 -0300

    ejercicio mysql

--- comandos.sql
+++ comandos.sql
@@ -2,6 +2,7 @@
 SHOW DATABASES;
 
 CREATE DATABASE primer_db;
+
 use primer_db;
 
 CREATE TABLE productos(
@@ -13,9 +14,33 @@ CREATE TABLE productos(
 SELECT * FROM productos;
 
 SHOW TABLES;
-
+INSERT INTO productos (tamanio)VALUES ()
 INSERT INTO productos (precio,nombre)VALUES (500000.123, "notebook");
 INSERT INTO productos (precio,nombre)VALUES (0.230, "notebook ojala");
-
+INSERT INTO productos (precio,nombre,tamanio,unidad)VALUES (350000.00, "PC GAMER",10,"KG");
 INSERT INTO productos ()VALUES ();
 INSERT INTO productos ( nombre )VALUES ("algo");
+INSERT INTO productos (precio, nombre, tamanio, unidad) VALUES (0.230, "notebook ojala", 20, "L");
+ALTER TABLE productos
+ADD COLUMN tiene_oferta TINYINT(1) DEFAULT 0;
+
+ALTER TABLE productos ADD COLUMN peso FLOAT;
+ALTER TABLE productos ADD COLUMN tamanio FLOAT NOT NULL;
+
+ALTER TABLE productos ADD COLUMN unidad VARCHAR(3) NOT NULL;
+
+UPDATE productos
+SET peso = 2 WHERE id = 1;
+
+UPDATE productos
+SET peso = 2 WHERE id = 1;
+
+UPDATE productos
+SET peso = 4 WHERE id = 5;
+
+UPDATE productos
+SET unidad = "KG" WHERE id = 1;
+
+
+DELETE FROM productos WHERE id=3; 
+DELETE FROM productos WHERE id=2; 
